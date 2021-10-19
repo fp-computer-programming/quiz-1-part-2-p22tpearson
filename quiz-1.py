aae3c9804375ef86593e3def6d06df9b10e60b80
p22tpearson <p22tpearson@fairfieldprep.org>
2021-10-19 10:51:50 -0400

    quiz part 2

---
+++ quiz-1.py
@@ -0,0 +1,27 @@
+#author: cjp 10/19/21
+
+h = (input("the day of the week is"))
+q = (input("the day of the month is"))
+k = (input("the year of the century is"))
+
+j = "year" //100
+k = "year" % 100
+
+if "day" == "0":
+    print("saturday")
+elif "day" == "1" :
+    print("sunday")
+elif "day" == "2":
+     print ("monday")
+elif "day" == "3":
+    print ("tuesday")
+elif "day" == "4":
+    print ("wednesday")
+elif "day" == "5": 
+    print ("thursday")
+elif "day" == "6":
+    print ("friday")
+
+
+
+
